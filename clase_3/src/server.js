5f65d1285328eb01f10ee2d290dc4d526c614c70
Martin <mfkutz@gmail.com>
2024-10-18 10:24:45 -0300

    middleware error & npm update mongoose

--- clase_3/src/server.js
+++ clase_3/src/server.js
@@ -3,6 +3,8 @@ import morgan from "morgan";
 import indexRoutes from "./routes/index.js";
 import { connectDB } from "./config/db.connect.js";
 import { config } from "./config/config.js";
+import compression from "express-compression";
+import errorHandler from "./middleware/errorHandler.js";
 
 const app = express();
 const PORT = config.PORT;
@@ -11,6 +13,11 @@ const PORT = config.PORT;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
+app.use(
+  compression({
+    brotli: { enabled: true, zlib: {} },
+  })
+);
 
 //Mongoose Database
 connectDB();
@@ -20,6 +27,9 @@ app.use("/api", indexRoutes);
 app.use((req, res) => {
   res.status(404).json({ response: "Route don't exist" });
 });
+
+//Errors Manager "next(error)"
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server online in port http://localhost:${PORT}`);
